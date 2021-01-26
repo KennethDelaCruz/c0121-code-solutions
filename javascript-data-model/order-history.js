35ff518f61f75ea1fbe46c58dc61f76143ca6992
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-01-26 12:45:04 -0800

    javascript-data-model/ edit 2!

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,14 +2,13 @@ var orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
     totalPrice: 34.00,
-    totalItems: 1,
     sentTo: 'JS Masher',
     orderNumber: '144-3941689-8772232',
     delivered: 'Aug 8, 2020',
     directHand: false,
     items: [{
       name: 'JavaScript for impatient programmers',
-      by: 'Rauschmayer, Dr Axel',
+      author: 'Rauschmayer, Dr Axel',
       returnWindowClose: 'Return window closed on Sep 7, 2020',
       price: 34.00
     }]
@@ -17,14 +16,13 @@ var orderHistory = [
   {
     orderPlaced: 'July 19, 2020',
     totalPrice: 44.53,
-    totalItems: 1,
     sentTo: 'JS Masher',
     orderNumber: '113-9984268-1280257',
     delivered: 'Jul 20,2020',
     directHand: true,
     items: [{
       name: 'The Timeless Way of Building',
-      by: 'Alexander, Christopher',
+      author: 'Alexander, Christopher',
       returnWindowClose: 'Return window closed on Aug 19, 2020',
       price: 41.33
     }]
@@ -32,14 +30,12 @@ var orderHistory = [
   {
     orderPlaced: 'July 4, 2020',
     totalPrice: 17.22,
-    totalItems: 1,
     sentTo: 'JS Masher',
     orderNumber: '114-2875557-9059409',
     delivered: 'July 7, 2020',
     directHand: true,
     items: [{
       name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and vibration Features. No Driver and No Lag-Gamecube Adapter',
-      by: '',
       returnWindowClose: 'Return window closed on Aug 5, 2020',
       price: 15.98
     }]
@@ -47,7 +43,6 @@ var orderHistory = [
   {
     orderPlaced: 'July 3, 2020',
     totalPrice: 138.93,
-    totalItems: 2,
     sentTo: 'JS Masher',
     orderNumber: '113-2883177-2648248',
     delivered: 'Jul 5, 2020',
@@ -55,7 +50,6 @@ var orderHistory = [
     items: [
       {
         name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-        by: '',
         returnWindowClose: 'Return window Close on Aug 4,2020',
         price: 94.95
       },
