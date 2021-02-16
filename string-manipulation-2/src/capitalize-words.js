904911506b1d017008fee7f2101d3e7f1947e05a
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-15 18:44:02 -0800

    finished the first 2 exercises. currently on the 3rd one.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,20 @@
 /* exported capitalizeWords */
+function capitalizeWord(word) {
+  var lower = word.toLowerCase();
+  var newWord = []
+  var splitWord = word.split('');
+  if (lower === 'javascript'){
+    return 'JavaScript';
+  }
+
+  for (var i = 0; i < splitWord.length; i++){
+    if (i === 0){
+      var capital = splitWord[i].toUpperCase();
+      newWord.push(capital);
+    } else {
+      var lowerLetter = splitWord[i].toLowerCase();
+      newWord.push(lowerLetter)
+    }
+  }
+  return newWord.join('')
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,20 @@
 /* exported ransomCase */
+function ransomCase(string){
+  var lowercase = string.toLowerCase();
+  var something = lowercase.split('');
+  var newWord = [];
+  console.log(something);
+
+  for (var i = 0; i < something.length; i++)
+  var evenOdd = i % 2;
+  if (evenOdd === 0) {
+  var capital = something[i].toUpperCase();
+    newWord.push(capital);
+  } else {
+  var lowered = something[i].toLowerCase();
+    newWord.push(lowered);
+  }
+
+  return newWord.join('')
+
+}
