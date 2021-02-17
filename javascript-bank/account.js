926c7d11f390013a08d536ef5ddfc644016f7da7
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-16 23:08:16 -0800

    Completed javascript-bank challenge

---
+++ javascript-bank/account.js
@@ -0,0 +1,47 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  var transactionsList = this.transactions;
+  var newTransaction = new Transaction('deposit', amount);
+
+  if (amount > 0) {
+    transactionsList.push(newTransaction);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  var transactionsList = this.transactions;
+  var newTransaction = new Transaction('withdraw', amount);
+  if (amount > 0) {
+    transactionsList.push(newTransaction);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var transactionsList = this.transactions;
+  var balance = 0;
+  if (transactionsList.length === 0) {
+    return 0;
+  }
+
+  for (var i = 0; i < transactionsList.length; i++) {
+    if (transactionsList[i].type === 'deposit') {
+      balance += transactionsList[i].amount;
+    } else if (transactionsList[i].type === 'withdraw') {
+      balance -= transactionsList[i].amount;
+    }
+  }
+  return balance;
+
+};
---
+++ javascript-bank/bank.js
@@ -0,0 +1,50 @@
+/* exported Bank */
+function Bank() {
+  this.accounts = [];
+  this.nextAccountNumber = 1;
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  var accountList = this.accounts;
+  var startBalance = balance;
+  if (balance > 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(startBalance);
+    accountList.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  var accountList = this.accounts;
+  var chosenAccount = 0;
+
+  for (var i = 0; i < accountList.length; i++) {
+    if (accountList[i].number === number) {
+      chosenAccount = accountList[i];
+    } else {
+      continue;
+    }
+  }
+
+  if (chosenAccount === 0) {
+    return null;
+  }
+  return chosenAccount;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var accountList = this.accounts;
+  var totalAssets = 0;
+  if (accountList === 0) {
+    return 0;
+  }
+  for (var i = 0; i < accountList.length; i++) {
+    totalAssets += accountList[i].getBalance();
+  }
+
+  return totalAssets;
+};
