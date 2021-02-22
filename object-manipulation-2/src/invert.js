2590511b3d0276d89654a2ab822077887499b832
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-02-21 19:28:45 -0800

    Completed object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,10 @@
 /* exported invert */
+function invert(source){
+  var newObject = {}
+  for (key in source){
+    var prop = source[key];
+    var value = key;
+    newObject[prop] = key;
+  }
+  return newObject
+}
