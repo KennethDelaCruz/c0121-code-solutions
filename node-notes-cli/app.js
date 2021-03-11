e1f9460164aefe82f47379a531ec2f3140216916
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-11 13:18:05 -0800

    added a new create.js file that will create new notes using the process.argv[3] = create

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,10 +1,14 @@
 const data = require('./data.json');
+const feature = process.argv[2];
 
-if (process.argv[2] === 'read') {
+if (feature === 'read') {
   const read = require('./read.js');
   if (process.argv[3]) {
     const index = process.argv[3];
     return console.log(`${index}: ${data.notes[index]}`);
   }
   read(data.notes);
+} if (feature === 'create') {
+  const create = require('./create.js');
+  create(process.argv[3]);
 }
