bf1688b90144760424e4baa02022e9a479ec5134
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 15:05:09 -0700

    completed express-post-json exercise

---
+++ express-post-json/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const newArray = [];
+  for (const prop in grades) {
+    newArray.push(grades[prop]);
+  }
+  res.json(newArray);
+});
+
+app.use(express.json());
+
+app.listen(3000, () => {
+});
+
+app.post('/api/grades', (req, res) => {
+  const newObject = {
+  };
+  newObject.id = nextId;
+  const reqObject = req.body;
+  for (const prop in reqObject) {
+    newObject[prop] = reqObject[prop];
+  }
+  nextId++;
+  res.json(newObject);
+});
