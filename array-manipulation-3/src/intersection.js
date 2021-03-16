30ef92eef37f479e360257148df7558f3d780f0f
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 13:01:44 -0700

    completed difference, unique and zip. workong on intersection

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,7 @@
 /* exported intersection */
+function intersection(first,second){
+  const concatArray = first.concat(second);
+  const newArray = [];
+  for(let i = 0; i < concatArray.length)
+
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,14 @@
 /* exported zip */
+function zip(first, second) {
+  const newArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (!first[i] || !second[i]) {
+      break;
+    }
+    const zipArray = [];
+    zipArray.push(first[i]);
+    zipArray.push(second[i]);
+    newArray.push(zipArray);
+  }
+  return newArray;
+}
