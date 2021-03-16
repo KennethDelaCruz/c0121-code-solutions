3cd296415ef1a79408afc7e9477c77fa87731cce
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 15:48:45 -0700

    Completed array manipulation 3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,7 +1,16 @@
 /* exported intersection */
-function intersection(first,second){
-  const concatArray = first.concat(second);
+function intersection(first, second) {
+  const catArray = first.concat(second);
   const newArray = [];
-  for(let i = 0; i < concatArray.length)
-
+  for (let i = 0; i < catArray.length; i++) {
+    const index = catArray[i];
+    if (first.includes(index) && second.includes(index)) {
+      if (newArray.includes(index)) {
+        continue;
+      }
+      newArray.push(index);
+    }
+    continue;
+  }
+  return newArray;
 }
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,15 @@
 /* exported union */
+function union(first, second) {
+  const concatArray = first.concat(second);
+  const newArray = [];
+
+  for (let i = 0; i < concatArray.length; i++) {
+    if (newArray.includes(concatArray[i])) {
+      continue;
+    } else {
+      newArray.push(concatArray[i]);
+    }
+  }
+
+  return newArray;
+}
