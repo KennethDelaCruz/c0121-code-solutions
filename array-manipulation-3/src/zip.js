6e602ab5041f75354a114a678ee867dfa86809e8
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 16:00:18 -0700

    added a guard on equal.js / determined the length of the array by finding the Math.min of two arrays

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,10 +1,8 @@
 /* exported zip */
 function zip(first, second) {
   const newArray = [];
-  for (let i = 0; i < first.length; i++) {
-    if (!first[i] || !second[i]) {
-      break;
-    }
+  const length = Math.min(first.length, second.length);
+  for (let i = 0; i < length; i++) {
     const zipArray = [];
     zipArray.push(first[i]);
     zipArray.push(second[i]);
