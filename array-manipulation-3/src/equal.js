1117d099673f797d7eff47c52bb11b48679f1600
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 10:10:54 -0700

    Completed first 2 problems of array manipulation3

---
+++ array-manipulation-3/src/equal.js
@@ -0,0 +1,11 @@
+/* exported equal */
+function equal(first, second) {
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    } else if (first.length !== second.length) {
+      return false;
+    }
+  }
+  return true;
+}
