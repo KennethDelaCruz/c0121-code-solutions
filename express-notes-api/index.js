8208da96d32258449d4e1b79b53e2fec3d727878
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-16 23:37:55 -0700

    finished app.post for this exercise

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const data = require('./data.json');
+const fs = require('fs');
+const { Console } = require('console');
 
 app.get('/api/notes', (req, res) => {
   const data = require('./data.json');
@@ -12,9 +15,30 @@ app.get('/api/notes/:id', (req, res) => {
   if (!parseInt(id) || id < 0) {
     res.status(400).send('ID must be a positive integer');
   } else if (!data.notes[id]) {
-    res.status(404).send('ID cannot be found');
+    res.status(404).send(`cannot find id ${id}`);
   }
   res.status(200).json(data.notes[id]);
+});
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).send('error: request does not exist');
+  }
+  const data = require('./data.json');
+  const newObject = {};
+  newObject.id = data.nextId;
+  for (const prop in req.body) {
+    newObject[prop] = req.body[prop];
+  }
+  data.notes[data.nextId] = newObject;
+  data.nextId++;
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) throw err;
+    res.status(201).send('File has been saved!');
+  });
+
 });
 
 app.listen(3000, () => {
