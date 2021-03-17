b739957521e5b63f83917cfa02c420a4808b8ff1
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-17 01:03:26 -0700

    Completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 
 app.get('/api/notes', (req, res) => {
   const data = require('./data.json');
-  res.status(200).json(data);
+  res.status(200).json(data.notes);
 });
 
 app.get('/api/notes/:id', (req, res) => {
   const data = require('./data.json');
   const id = req.params.id;
   if (!parseInt(id) || id < 0) {
-    res.status(400).send('ID must be a positive integer');
+    res.status(400).send({ error: 'ID must be a positive integer' });
   } else if (!data.notes[id]) {
     res.status(404).send(`cannot find id ${id}`);
   }
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.post('/api/notes', (req, res) => {
   if (Object.keys(req.body).length === 0) {
-    res.status(400).send('error: request does not exist');
+    res.status(400).send({ error: 'request does not exist' });
   }
   const data = require('./data.json');
   const newObject = {};
@@ -34,9 +34,9 @@ app.post('/api/notes', (req, res) => {
   data.nextId++;
   fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
     if (err) {
-      res.status(500).send('error: there is no such file or directory');
+      res.status(500).send({ error: 'there is no such file or directory' });
     }
-    res.status(201).send('File has been saved!');
+    res.status(201).json(newObject);
   });
 });
 
@@ -44,17 +44,38 @@ app.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   const data = require('./data.json');
   if (!data.notes[id]) {
-    res.status(404).send('error: notes id does note exist');
+    res.status(404).send({ error: 'notes id does note exist' });
   }
   delete data.notes[id];
   fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
     if (err) {
-      res.status(500).send('error: unexpected error has occured');
+      res.status(500).send({ error: 'unexpected error has occured' });
     }
-    res.status(204).send('request note id has been deleted');
+    res.status(204).send('');
   });
 });
 
 app.listen(3000, () => {
-  console.log('server is active');
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const data = require('./data.json');
+  let content = '';
+  for (const prop in req.body) {
+    content = prop;
+  }
+  if (Object.keys(req.body).length === 0 || !data.notes[id]) {
+    res.status(400).send({ error: 'notes/id doesnt exist OR there is no content to send' });
+  } else if (!data.notes[id][content]) {
+    res.status(400).send({ error: 'notes/id content does not exist' });
+  }
+  data.notes[id][content] = req.body[content];
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', err => {
+    if (err) {
+      res.status(500).send({ error: 'unexpected error has occured' });
+    }
+    res.status(200).json(data.notes);
+  });
+
 });
