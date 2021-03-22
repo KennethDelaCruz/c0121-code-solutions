415f08ab0eff8d1fa7f56c202d801ac12dcbd809
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-22 12:18:28 -0700

    completed array-map exercise

---
+++ array-map/main.js
@@ -0,0 +1,26 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const double = numbers.map(number => number * 2);
+console.log(double);
+
+const prices = numbers.map(number => {
+  return `$${number}.00`;
+});
+console.log(prices);
+
+const upperCase = languages.map(language => language.toUpperCase());
+console.log(upperCase);
+
+const firstLetters = languages.map(language => {
+  const $word = language.charAt(0).toUpperCase() + language.slice(1);
+  return $word;
+});
+console.log(firstLetters);
