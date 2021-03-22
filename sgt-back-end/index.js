8e33b721818070dafabf5c3f687292d65aead59e
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-21 21:56:50 -0700

    Completed sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.use(express.json());
 app.listen(3000, () => {
-  console.log('server is active');
 });
 
 const db = new pg.Pool({
@@ -65,9 +64,14 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
 });
 
 app.post('/api/grades', (req, res, next) => {
-  if (!req.body.name || !req.body.course || !req.body.score || req.body.score <= 0) {
+  const gradeId = parseInt(req.params.gradeId, 10);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
     return res.status(400).json({
-      error: 'Invalid Data Values OR not all Data requirements met'
+      error: 'invalid gradeId'
+    });
+  } else if (!req.body.name || !req.body.course || !req.body.score) {
+    return res.status(400).json({
+      error: 'Invalid Data Values OR not all data requirements met'
     });
   }
   const sql = `
@@ -92,6 +96,76 @@ app.post('/api/grades', (req, res, next) => {
       console.error(err);
       res.status(500).json({
         error: 'An Unexpected error occurred.'
+      });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res, next) => {
+  if (!req.params.gradeId || req.params.gradeId <= 0) {
+    return res.status(404).json({
+      error: 'Invalid gradeID OR gradeId does not exist'
+    });
+  } else if (req.body.score <= 0) {
+    return res.status(404).json({
+      error: 'Invalid score, score must be a non-negative number'
+    });
+  } else if (!req.body.course) {
+    return res.status(400).json({
+      error: 'score is missing/does not exist'
+    });
+  }
+  const sql = `
+  update "grades"
+      set "score" = $1
+    where "gradeId" = $2 AND "name" = $3 AND "course" = $4
+    returning *;
+    `;
+  const params = [req.body.score, req.params.gradeId, req.body.name, req.body.course];
+  db.query(sql, params)
+    .then(result => {
+      const updateRow = result.rows[0];
+      if (!updateRow) {
+        return res.status(404).json({
+          error: 'update failed, gradeId and name may not match/exist'
+        });
+      } else {
+        return res.status(200).json(updateRow);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'unexpected error occurred'
+      });
+    });
+});
+
+app.delete('/api/grades/:gradeId', (req, res, next) => {
+  const gradeId = parseInt(req.params.gradeId, 10);
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    return res.status(400).json({
+      error: 'invalid gradeId'
+    });
+  } else if (!req.body.name || !req.body.course) {
+    return res.status(400).json({
+      error: 'name and/or course must be provided'
+    });
+  }
+  const sql = `
+  delete from "grades"
+  where "gradeId" = $1 AND "name" = $2 AND "course" = $3;
+  `;
+  const params = [req.params.gradeId, req.body.name, req.body.course];
+  db.query(sql, params)
+    .then(result => {
+      return res.status(204).json({
+        success: 'requested gradeId deleted'
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({
+        error: 'unexpected error occurred'
       });
     });
 });
