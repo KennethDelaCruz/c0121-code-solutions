a42549eea9de047df2167ee36f370173949d0c4d
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-25 11:57:35 -0700

    completed react-hot-button-component

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomButton from './hot-button';
+
+ReactDOM.render(
+  <CustomButton text="hot-button"/>,
+  document.querySelector('#root')
+);
