259fb03d8a7ecca4b3d0c25221fecc7f4294482b
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-27 13:47:18 -0700

    completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,30 +3,32 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { selected: null };
     this.handleClick = this.handleClick.bind(this);
-    this.state = { view: null };
   }
 
-  handleClick(e) {
-    const value = e.target.getAttribute('view');
-    console.log(typeof value);
-    this.setState({ view: value });
+  handleClick(i) {
+    if (this.state.selected === i) {
+      this.setState({ selected: null });
+    } else {
+      this.setState({ selected: i });
+    }
 
   }
 
   render() {
-
-    const element = this.props.info.map(object => {
-      const test = (this.state.view === object.name ? 'description' : 'description hidden');
-
-      return (
-        <div key={object.name}>
-          <div onClick={this.handleClick} view={object.language}className="language">{object.name}</div>
-          <div className={test}>{object.description}</div>
-        </div>
-      );
-    });
-    return element;
+    const selected = this.state.selected;
+    return (
+      <div className="accordion">
+        {this.props.info.map((object, i) => {
+          return (
+            <>
+              <div className="item" onClick={() => this.handleClick(i)}>{object.name}</div>
+              <div className={selected === i ? 'description' : 'hidden description'}>{object.description}</div>
+            </>
+          );
+        })}
+      </div>
+    );
   }
-
 }
