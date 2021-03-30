501ac03d22354305ced77c2ee3609de3d756b83b
Kenneth Dela Cruz <kennethdc2693@gmail.com>
2021-03-30 15:07:29 -0700

    completed react-app-drawer-component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 export default class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { toggle: false };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    if (this.state.toggle) {
+      this.setState({ toggle: false });
+    } else {
+      this.setState({ toggle: true });
+    }
+  }
 
   render() {
     return (
       <div className="container">
-        <i className="fas fa-bars"></i>
-        <div className="modal">
+        <i className="fas fa-bars" onClick={this.handleToggle}></i>
+        <div className={this.state.toggle ? 'modal' : 'modal hidden'} onClick={this.handleToggle}>
           <div className="drawer">
             <h2>Menu</h2>
             <p>About</p>
